5fb1756d0e46c9e6e2114eff4ff45e085f279c2d
Saranithan <saransaranithan22@gmail.com>
2025-03-15 12:01:15 +0530

    Add files via upload

---
+++ Day 01/Class Work/Exercise 04/Ex04.java
@@ -0,0 +1,17 @@
+class StuDetails {
+	public static void main(String [] args)
+	{
+		System.out.println("Name: Sathananthamoorthy Saranithan");
+		System.out.println("Registration Number:2022/ASP/82");
+		System.out.println("Degree Program: Bsc.Applied Mathematics and Computing");
+		System.out.println();
+		
+		String Name="Sathananthamoorthy sranithan";
+		String RegNo="2022/ASP/82";
+		String Degree="Bsc.Applied Mathematics and Computing";
+		
+		System.out.println("Full Name: "+Name);
+		System.out.println("Registration Number: "+RegNo);
+		System.out.println("Degree Program: "+Degree);
+	}
+}
